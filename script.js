af2f73ada2169e8c49027591bdf7f0f52086f385
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-27 01:12:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let inp = document.getElementById('fname');
+document.addEventListener('click',(e)=>{
+	let original = inp.value;
+	inp.value=original.toUpperCase();
+});
